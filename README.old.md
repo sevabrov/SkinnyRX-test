ed1b5589f5ad3995e48ca075e678213416d45cdb
brovarny <ext-vsevolod.brovarnyi@here.com>
2024-08-20 19:13:49 +0200

    [Step-1]: Basic application setup
    - setup basic react with typescript
    - setup MUI
    - delete unused files

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# SkinnyRX-test
+test task for Senior React TypeScript Developer Role
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: '400px', m: '0 auto' }}>
+      <Typography sx={{ mt: 2, mb: 1, textAlign: 'center' }}>
+        skinny-rx-test
+      </Typography>
+    </Box>
+  );
+}
+
+export default App;
